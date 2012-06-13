5163cd201b5d7d79a9e3127ec319c43ca3cfbc92
johnmmcparland@gmail.com <johnmmcparland@gmail.com@f7669c04-fe19-7c60-3fb4-0f81449db4e7>
2012-06-13 15:52:29 +0000

    It Works!
    Main changes;
    
    1) Removed dev/prod configuration for log4j and the Spring properties and now use just one.  The web application does not start up with VM arguments so these could not be used.
    2) Added additional copy task in the build.xml to copy over the log4j and Spring properties as well as the Spring configuration XML file.
    3) Renamed TestDb to RecommendRole to be more descriptive.  Credit given to Dariusz Majewski for writing the class.
    4) Added more detail to index.html so that the user knows how to use the application.
    5) Corrected the location of the log4j properties in web.xml
    6) Renamed the project "myfmr" from "FootballManagerRoles".  May think of something better later.
    7) Added more 3rd party jars which were missing but required for the file upload part.

---
+++ FootballManagerRoles/main/packages/com/mcparland/john/footballmanagerroles/servlets/RecommendRole.java
@@ -0,0 +1,71 @@
+package com.mcparland.john.footballmanagerroles.servlets;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.beanutils.BeanUtilsBean2;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.mcparland.john.footballmanagerroles.FootballManagerRoles;
+import com.mcparland.john.footballmanagerroles.recommend.PlayerRecommendations;
+
+/**
+ * Servlet implementation class TestDb
+ * 
+ * @author Dariusz Majewski
+ */
+@WebServlet("/RecommendRole")
+public class RecommendRole extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @SuppressWarnings("unchecked")
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        FootballManagerRoles footballManagerRoles = (FootballManagerRoles) WebApplicationContextUtils
+                .getWebApplicationContext(getServletContext()).getBean("footballManagerRoles");
+
+        // Create a factory for disk-based file items
+        FileItemFactory factory = new DiskFileItemFactory();
+
+        // Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        response.getWriter().append("<html><body>");
+        // Parse the request
+        try {
+            List<FileItem> items = upload.parseRequest(request);
+            for (FileItem item : items) {
+                response.getWriter().append("<hr/>");
+                File f = File.createTempFile("fmr_tmp_", item.getName());
+                item.write(f);
+                PlayerRecommendations pr = footballManagerRoles.process(f);
+                f.delete();
+                response.getWriter().append(
+                        BeanUtilsBean2.getInstance().describe(pr.getPlayer()).toString().replaceAll(",", "<br/>"));
+                response.getWriter().append("<br/><br/>Recomendations: <br/>");
+                response.getWriter().append(
+                        pr.getRecommendations().getRecommendations().toString().replaceAll(",", "<br/>"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().append(e.getLocalizedMessage());
+        }
+        response.getWriter().append("</body></html>");
+        response.getWriter().close();
+    }
+
+}
